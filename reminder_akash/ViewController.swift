78fd379e4c5bef7593bd25631ab75d163f482de1
akash <akashs@gmail.com>
2024-02-27 10:41:02 +0530

    Initial Commit

---
+++ reminder_akash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reminder_akash
+//
+//  Created by Shruti Makwana on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reminder_akashUITests/reminder_akashUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  reminder_akashUITestsLaunchTests.swift
+//  reminder_akashUITests
+//
+//  Created by Shruti Makwana on 27/02/24.
+//
+
+import XCTest
+
+final class reminder_akashUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
